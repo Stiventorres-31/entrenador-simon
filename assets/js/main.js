e210234ac83e4228fafdf4a3b1ea286190fb2680
Stiven Torres <torresmendozastiven@gmail.com>
2025-09-23 22:17:34 -0500

    subiendo todo

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    hamburger.classList.toggle('active');
+});
+
+// Opcional: Cerrar el menú al hacer clic en un enlace
+document.querySelectorAll('.nav-links li a').forEach(link => {
+    link.addEventListener('click', () => {
+        navLinks.classList.remove('active');
+        hamburger.classList.remove('active');
+    });
+});
